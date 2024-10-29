e24af13ff42afaee7896607dd407e1ea30bf3197
DEV-COM <kwp@tlen.pl>
2024-10-29 11:17:05 +0100

    nowy projekt 1

--- src/test/groovy/LateInvoiceNotifierSpec.groovy
+++ src/test/groovy/LateInvoiceNotifierSpec.groovy
@@ -18,4 +18,27 @@ class LateInvoiceNotifierSpec extends Specification {
         sampleCustomer.setLastName("Kowalski")
     }
 
+    def "a late invoice should trigger an email"() {
+        given: "a customer with a late invoice"
+        invoiceStorage.hasOutstandingInvoice(sampleCustomer) >> true
+
+        when: "we check if an emial should be sent"
+        lateInvoiceNotifier.notifyIfLate(sampleCustomer)
+
+        then: "the customer is indeed emailed"
+        1 * emailSender.sendEmail(sampleCustomer)
+//        verify(emailSender, times(1).sendEmail(sampleCustomer);
+    }
+
+    def "no late invoices"() {
+        given: "a customer with a good invoice"
+        invoiceStorage.hasOutstandingInvoice(sampleCustomer) >> false
+
+        when: "we check if an emial should be sent"
+        lateInvoiceNotifier.notifyIfLate(sampleCustomer)
+
+        then: "the customer is indeed emailed"
+        0 * emailSender.sendEmail(sampleCustomer)
+    }
+
 }
